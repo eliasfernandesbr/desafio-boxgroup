0be09c6dd3fbe81677285c1461f6d0cbbc3d9e9a
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-06-23 18:33:05 -0300

    add first routes, app listen and script to start

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const port = 5000;
+
+
+app.get('/api/users', (req, res)=>{
+    res.send('teste API')
+})
+
+app.get('/', (req, res) =>{
+    res.json({message: 'hello world'})
+})
+
+
+app.listen(port, ()=>{
+    console.log('🔸START BACKEND SERVER🔸')
+})
