0eaebb4ed594478418da8785a0f93cf1c132e2b4
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-06-23 18:50:49 -0300

    getting github langagues from backend route

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [languages, setLanguages] = useState([]);
+  const [selectedLang, setLang] = useState("");
+
+
+  const getLanguages = async () => {
+    const res = await axios.get("http://localhost:5000/api/languages");
+    console.log(res.data);
+    setLanguages(res.data);
+  };
+
+
+  useEffect(() => {
+    getLanguages();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select className='selectLang' value={selectedLang} onChange={(e) => setLang(e.target.value)}>
+        <option value="">Escolha uma linguagem</option>
+        {languages.map((language) => (
+          <option value={language.name}>{language.name}</option>
+        ))}
+      </select>
+     
+      
     </div>
   );
 }
