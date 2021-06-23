4aec4368da1570377b644d1f34181c5ee3e71fae
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-06-23 18:51:41 -0300

    creating route to get repos from github api

--- src/server.js
+++ src/server.js
@@ -19,6 +19,17 @@ app.get('/api/languages', async (req, res)=>{
     }
 })
 
+app.get('/api/repos/:lang', async (req, res)=>{
+    const lang = req.params.lang
+    console.log('Estou aqui')
+    console.log(lang)
+    try{
+        const {data} = await axios.get(`https://api.github.com/search/repositories?q=language:${lang}&order=desc`);
+        res.send(data)
+    }catch(error){
+        console.log(error)
+    }
+})
 
 
 app.get('/api/users', (req, res)=>{
